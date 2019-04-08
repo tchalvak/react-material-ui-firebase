d0f67ccf2e5b379483068fa896eee807adc479c5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-08 12:05:16 +0000

    Deploy Phoqe/react-material-ui-firebase to github.com/Phoqe/react-material-ui-firebase.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-material-ui-firebase/precache-manifest.ec849dbfd1bc24976491acfd8d0eead7.js"
+  "/react-material-ui-firebase/precache-manifest.6eec8eb2cb9f28dee0d3c7eaa5455d9c.js"
 );
 
 workbox.clientsClaim();
