32ec4df165b0872c62ccc6441fe2ea92e7aaf0d5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-30 18:42:06 +0000

    Deploy Phoqe/react-material-ui-firebase to github.com/Phoqe/react-material-ui-firebase.git:gh-pages

--- precache-manifest.5ca26c4d92c80dfef1209d633523fb60.js
+++ precache-manifest.5ca26c4d92c80dfef1209d633523fb60.js
@@ -1,23 +1,27 @@
-self.__precacheManifest = [
+self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
-    "url": "/react-material-ui-firebase/static/media/roboto-latin-400italic.d8bcbe72.woff2"
+    "revision": "274ebfb3bb65b484b0963667243b6b3f",
+    "url": "/react-material-ui-firebase/index.html"
   },
   {
-    "revision": "fd73073d0d5c89154421",
-    "url": "/react-material-ui-firebase/static/css/main.85820cd4.chunk.css"
+    "revision": "9c24da3cf102a7cc8423",
+    "url": "/react-material-ui-firebase/static/css/2.ff8d51eb.chunk.css"
   },
   {
-    "revision": "bc833e725c137257c2c42a789845d82f",
-    "url": "/react-material-ui-firebase/static/media/roboto-latin-900italic.bc833e72.woff"
+    "revision": "969df85f66f09e7e9549",
+    "url": "/react-material-ui-firebase/static/css/main.795991f2.chunk.css"
   },
   {
-    "revision": "cc06db1477263e26a0bf",
-    "url": "/react-material-ui-firebase/static/js/1.cc06db14.chunk.js"
+    "revision": "9c24da3cf102a7cc8423",
+    "url": "/react-material-ui-firebase/static/js/2.88faf258.chunk.js"
   },
   {
-    "revision": "2aa80d779d8e031c2e99",
-    "url": "/react-material-ui-firebase/static/js/runtime~main.2aa80d77.js"
+    "revision": "969df85f66f09e7e9549",
+    "url": "/react-material-ui-firebase/static/js/main.2c4e4a27.chunk.js"
+  },
+  {
+    "revision": "01068b09c0b3a06fc018",
+    "url": "/react-material-ui-firebase/static/js/runtime~main.438f6793.js"
   },
   {
     "revision": "987b84570ea69ee660455b8d5e91f5f1",
@@ -44,12 +48,12 @@ self.__precacheManifest = [
     "url": "/react-material-ui-firebase/static/media/roboto-latin-300.a1471d1d.woff"
   },
   {
-    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
-    "url": "/react-material-ui-firebase/static/media/roboto-latin-300italic.d69924b9.woff2"
-  },
-  {
     "revision": "210a7c781f5a354a0e4985656ab456d9",
     "url": "/react-material-ui-firebase/static/media/roboto-latin-300italic.210a7c78.woff"
+  },
+  {
+    "revision": "d69924b98acd849cdeba9fbff3f88ea6",
+    "url": "/react-material-ui-firebase/static/media/roboto-latin-300italic.d69924b9.woff2"
   },
   {
     "revision": "5d4aeb4e5f5ef754e307d7ffaef688bd",
@@ -60,12 +64,12 @@ self.__precacheManifest = [
     "url": "/react-material-ui-firebase/static/media/roboto-latin-400.bafb105b.woff"
   },
   {
-    "revision": "fd73073d0d5c89154421",
-    "url": "/react-material-ui-firebase/static/js/main.fd73073d.chunk.js"
-  },
-  {
     "revision": "9680d5a0c32d2fd084e07bbc4c8b2923",
     "url": "/react-material-ui-firebase/static/media/roboto-latin-400italic.9680d5a0.woff"
+  },
+  {
+    "revision": "d8bcbe724fd6f4ba44d0ee6a2675890f",
+    "url": "/react-material-ui-firebase/static/media/roboto-latin-400italic.d8bcbe72.woff2"
   },
   {
     "revision": "285467176f7fe6bb6a9c6873b3dad2cc",
@@ -112,11 +116,7 @@ self.__precacheManifest = [
     "url": "/react-material-ui-firebase/static/media/roboto-latin-900italic.7b770d6c.woff2"
   },
   {
-    "revision": "cc06db1477263e26a0bf",
-    "url": "/react-material-ui-firebase/static/css/1.d45b7692.chunk.css"
-  },
-  {
-    "revision": "02670e0d10485282c00afc5a0c44c766",
-    "url": "/react-material-ui-firebase/index.html"
+    "revision": "bc833e725c137257c2c42a789845d82f",
+    "url": "/react-material-ui-firebase/static/media/roboto-latin-900italic.bc833e72.woff"
   }
-];
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-material-ui-firebase/precache-manifest.d00dfb23f495342420507c741e364148.js"
+  "/react-material-ui-firebase/precache-manifest.5ca26c4d92c80dfef1209d633523fb60.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-material-ui-firebase/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-material-ui-firebase/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
