a4202e3c4e8c6ab9457e92516894a862a71babe3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-30 08:39:04 +0000

    Deploy Phoqe/react-material-ui-firebase to github.com/Phoqe/react-material-ui-firebase.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-material-ui-firebase/precache-manifest.34a09de9c191880195821d1fd5c2ad71.js"
+  "/react-material-ui-firebase/precache-manifest.fce0681736ac8614df4dc2c7253d6201.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-material-ui-firebase/index.html"), {
+workbox.routing.registerNavigationRoute("/react-material-ui-firebase/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
