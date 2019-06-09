6f776f80a8fc31fbe74cb2b8234dd2d61b943bae
Linus Långberg <phoqe@phoqe.dev>
2019-06-09 14:52:11 +0200

    Add base dialog

--- src/dialogs/SettingsDialog/SettingsDialog.js
+++ src/dialogs/SettingsDialog/SettingsDialog.js
@@ -2,28 +2,42 @@ import React, { Component } from 'react';
 
 import PropTypes from 'prop-types';
 
-import { withStyles } from '@material-ui/core/styles';
-
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogActions from '@material-ui/core/DialogActions';
 
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Button from '@material-ui/core/Button';
-import Hidden from '@material-ui/core/Hidden';
 
-import SwipeableViews from 'react-swipeable-views';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import PaletteIcon from '@material-ui/icons/Palette';
+import LinkIcon from '@material-ui/icons/Link';
+import SecurityIcon from '@material-ui/icons/Security';
 
-import AccountTab from '../../tabs/settings/AccountTab/AccountTab';
-import AppearanceTab from '../../tabs/settings/AppearanceTab/AppearanceTab';
+const tabs = [
+  {
+    key: 'account',
+    icon: <AccountCircleIcon />,
+    label: 'Account'
+  },
 
-const styles = (theme) => ({
-  tabs: {
-    marginBottom: theme.spacing(1)
+  {
+    key: 'appearance',
+    icon: <PaletteIcon />,
+    label: 'Appearance'
+  },
+
+  {
+    key: 'connections',
+    icon: <LinkIcon />,
+    label: 'Connections'
+  },
+
+  {
+    key: 'security',
+    icon: <SecurityIcon />,
+    label: 'Security'
   }
-});
+];
 
 class SettingsDialog extends Component {
   constructor(props) {
@@ -42,199 +56,50 @@ class SettingsDialog extends Component {
     }
 
     if (key === 'Enter') {
-      this.props.onClose();
+      this.props.onOkClick();
     }
   };
 
-  changeTab = (event, value) => {
+  handleTabChange = (event, value) => {
     this.setState({
       selectedTab: value
     });
   };
 
-  changeIndex = (index) => {
-    this.setState({
-      selectedTab: index
-    });
-  };
-
-  handleResetClick = () => {
-    const { primaryColor, secondaryColor, type, defaultTheme } = this.props;
-
-    if (primaryColor !== defaultTheme.primaryColor || secondaryColor !== defaultTheme.secondaryColor || type !== defaultTheme.type) {
-      setTimeout(this.props.onResetClick, 137.5);
-    }
-  };
-
   render() {
-    // Styling
-    const { classes } = this.props;
+    // Dialog Properties
+    const { fullScreen, open } = this.props;
 
-    // Properties
-    const {
-      fullScreen,
-      open,
-      user,
-      isPerformingAuthAction,
-      isVerifyingEmailAddress,
-      colors,
-      types,
-      primaryColor,
-      secondaryColor,
-      type,
-      defaultTheme
-    } = this.props;
-
-    // Events
-    const {
-      onClose,
-      onAddAvatarClick,
-      onChangeAvatarClick,
-      onAddDisplayNameClick,
-      onChangeDisplayNameClick,
-      onAddEmailAddressClick,
-      onVerifyEmailAddressClick,
-      onPrimaryColorChange,
-      onSecondaryColorChange,
-      onTypeChange
-    } = this.props;
+    // Dialog Events
+    const { onClose } = this.props;
 
     const { selectedTab } = this.state;
 
-    let hasDeviatedFromDefaultSettings = false;
-
-    if (defaultTheme) {
-      hasDeviatedFromDefaultSettings = (
-        primaryColor !== defaultTheme.primaryColor ||
-        secondaryColor !== defaultTheme.secondaryColor ||
-        type !== defaultTheme.type
-      );
+    if (!onClose) {
+      return null;
     }
 
     return (
       <Dialog fullScreen={fullScreen} open={open} onClose={onClose} onKeyPress={this.handleKeyPress}>
         <DialogTitle>Settings</DialogTitle>
 
-        <Tabs className={classes.tabs} indicatorColor="primary" textColor="primary" onChange={this.changeTab} value={selectedTab} variant="fullWidth">
-          <Tab label="Account" />
-          <Tab label="Appearance" />
+        <Tabs indicatorColor="primary" textColor="primary" value={selectedTab} onChange={this.handleTabChange}>
+          {tabs.map((tab) => {
+            return (
+              <Tab key={tab.key} icon={tab.icon} label={tab.label} />
+            );
+          })}
         </Tabs>
-
-        <DialogContent>
-          <Hidden only="xs">
-            {selectedTab === 0 &&
-              <AccountTab
-                user={user}
-                isPerformingAuthAction={isPerformingAuthAction}
-                isVerifyingEmailAddress={isVerifyingEmailAddress}
-                onAddAvatarClick={onAddAvatarClick}
-                onChangeAvatarClick={onChangeAvatarClick}
-                onAddDisplayNameClick={onAddDisplayNameClick}
-                onChangeDisplayNameClick={onChangeDisplayNameClick}
-                onAddEmailAddressClick={onAddEmailAddressClick}
-                onVerifyEmailAddressClick={onVerifyEmailAddressClick}
-              />
-            }
-
-            {selectedTab === 1 &&
-              <AppearanceTab
-                colors={colors}
-                types={types}
-                primaryColor={primaryColor}
-                secondaryColor={secondaryColor}
-                type={type}
-                onPrimaryColorChange={onPrimaryColorChange}
-                onSecondaryColorChange={onSecondaryColorChange}
-                onTypeChange={onTypeChange}
-              />
-            }
-          </Hidden>
-
-          <Hidden only={['sm', 'md', 'lg', 'xl']}>
-            <SwipeableViews index={selectedTab} onChangeIndex={this.changeIndex}>
-              <AccountTab
-                user={user}
-                isPerformingAuthAction={isPerformingAuthAction}
-                isVerifyingEmailAddress={isVerifyingEmailAddress}
-                onAddAvatarClick={onAddAvatarClick}
-                onChangeAvatarClick={onChangeAvatarClick}
-                onAddDisplayNameClick={onAddDisplayNameClick}
-                onChangeDisplayNameClick={onChangeDisplayNameClick}
-                onAddEmailAddressClick={onAddEmailAddressClick}
-                onVerifyEmailAddressClick={onVerifyEmailAddressClick}
-              />
-
-              <AppearanceTab
-                colors={colors}
-                types={types}
-                primaryColor={primaryColor}
-                secondaryColor={secondaryColor}
-                type={type}
-                onPrimaryColorChange={onPrimaryColorChange}
-                onSecondaryColorChange={onSecondaryColorChange}
-                onTypeChange={onTypeChange}
-              />
-            </SwipeableViews>
-          </Hidden>
-        </DialogContent>
-
-        {(selectedTab === 1 && hasDeviatedFromDefaultSettings) &&
-          <React.Fragment>
-            <Hidden only="xs">
-              <DialogActions>
-                <Button color="primary" variant="contained" onClick={this.handleResetClick}>Reset</Button>
-              </DialogActions>
-            </Hidden>
-
-            <Hidden only={['sm', 'md', 'lg', 'xl']}>
-              <DialogActions>
-                <Button color="primary" onClick={onClose}>Cancel</Button>
-                <Button color="primary" variant="outlined" onClick={this.handleResetClick}>Reset</Button>
-                <Button color="primary" variant="contained" onClick={onClose}>OK</Button>
-              </DialogActions>
-            </Hidden>
-          </React.Fragment>
-        }
-
-        {(selectedTab !== 1 || !hasDeviatedFromDefaultSettings) &&
-          <Hidden only={['sm', 'md', 'lg', 'xl']}>
-            <DialogActions>
-              <Button color="primary" onClick={onClose}>Cancel</Button>
-              <Button color="primary" variant="contained" onClick={onClose}>OK</Button>
-            </DialogActions>
-          </Hidden>
-        }
       </Dialog>
     );
   }
 }
 
 SettingsDialog.propTypes = {
-  classes: PropTypes.object.isRequired,
-
   fullScreen: PropTypes.bool,
   open: PropTypes.bool.isRequired,
 
-  user: PropTypes.object.isRequired,
-  isPerformingAuthAction: PropTypes.bool.isRequired,
-  isVerifyingEmailAddress: PropTypes.bool.isRequired,
-  colors: PropTypes.array.isRequired,
-  types: PropTypes.array.isRequired,
-  primaryColor: PropTypes.string.isRequired,
-  secondaryColor: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-
-  onClose: PropTypes.func.isRequired,
-  onAddAvatarClick: PropTypes.func.isRequired,
-  onChangeAvatarClick: PropTypes.func.isRequired,
-  onAddDisplayNameClick: PropTypes.func.isRequired,
-  onChangeDisplayNameClick: PropTypes.func.isRequired,
-  onAddEmailAddressClick: PropTypes.func.isRequired,
-  onVerifyEmailAddressClick: PropTypes.func.isRequired,
-  onPrimaryColorChange: PropTypes.func.isRequired,
-  onSecondaryColorChange: PropTypes.func.isRequired,
-  onTypeChange: PropTypes.func.isRequired,
-  onResetClick: PropTypes.func.isRequired
+  onClose: PropTypes.func.isRequired
 };
 
-export default withStyles(styles)(SettingsDialog);
+export default SettingsDialog;
